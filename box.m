d40280b7ff890d2577b305ad75dab094a92f76fa
valentin <v.marcon@outlook.fr>
2015-12-16 13:50:53 +0100

    Adding Box class(2)

---
+++ box.m
@@ -0,0 +1,19 @@
+classdef box
+    %BO Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        portVal
+        rackVal
+        offsetVal
+        pelletVal
+        nosePokeVal
+        leftLeverVal
+        rightLeverVal
+    end
+    
+    methods
+    end
+    
+end
+
